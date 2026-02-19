4d89a264385e949f0c25a75114e22c66220fcb15
Garik Asatryan <asa7ur@proton.me>
2026-02-19 17:37:57 +0100

    Initial commit

---
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_festival_api/DweseFestivalApiApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.GarikAsatryan.dwese_festival_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DweseFestivalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DweseFestivalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalixar/daw2/GarikAsatryan/dwese_festival_api/DweseFestivalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.GarikAsatryan.dwese_festival_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DweseFestivalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
